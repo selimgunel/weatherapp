2043371702e248657b4e2fb056b15ce742bf0f7a
Selim Günel <117628137+selimgunel@users.noreply.github.com>
2023-01-28 03:20:47 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,21 +9,37 @@ const city = document.querySelector('.city');
 const temp = document.querySelector('.temp');
 const desc = document.querySelector('.description');
 const wind = document.querySelector('.wind');
-const barcelona = {lat : 41.38, lon : 2.17}
-const amsterdam = {lat : 52.37, lon : 4.89}
-const brugge = {lat : 51.20, lon : 3.22}
-const stockholm = {lat : 59.32, lon : 18.07}
-const prague = {lat : 50.08, lon: 14.42}
+const today = document.querySelector(".today");
+const tomorrow = document.querySelector(".following");
+const tomorrowtemp = document.querySelector(".tomorrow");
+const othertomorrowtemp = document.querySelector(".othertomorrow");
+const followingcity = document.querySelector(".city2");
+const back = document.querySelector(".back");
+const firstbuton = document.querySelector("#butontoday");
+
+const coordinates = {
+    barcelona : {lat : 41.38, lon : 2.17},
+    amsterdam : {lat : 52.37, lon : 4.89},
+    brugge : {lat : 51.20, lon : 3.22},
+    stockholm : {lat : 59.32, lon : 18.07},
+    prague : {lat : 50.08, lon: 14.42}
+    
+}
+
+tomorrow.style.display = "none";
+firstbuton.style.display = "none";
 
 
-
-// Eventler 
+// Eventler
 
 //select list değiştiği zaman çalışacak event
 select.addEventListener("change", getCity);
 
 //önümüzdeki günlerin hava tahmini için click eventi
 follow.addEventListener("click", getFollowingDays);
+
+//back tuşuna basıldığında önceki sayfayı yükleyecek event
+back.addEventListener("click",getCity);
 
 
 function getCity(e) {
@@ -34,6 +50,10 @@ function getCity(e) {
 
         getInfo(select.options[select.selectedIndex].value);
     }
+
+    today.style.display = "block";
+    tomorrow.style.display = "none";
+    firstbuton.style.display = "block";
 
 }
 
@@ -49,10 +69,6 @@ function getInfo(cityname) {
             return data.json();
         })
         .then(info);
-    
-    follow.textContent = "Click here for following days";
-    
-    
 
 }
 
@@ -66,7 +82,7 @@ function info(apikey) {
 
     desc.textContent = `${apikey.weather[0].description}`;
 
-    wind.textContent = "Wind: " + Math.round(`${apikey.wind.speed}`) + " km/h";
+    wind.textContent = "wind: " + Math.round(`${apikey.wind.speed}`) + " km/h";
 }
 
 function getFollowingDays(e) {
@@ -74,34 +90,14 @@ function getFollowingDays(e) {
     // önümüzdeki günlerdeki hava tahmini için şehir sorgulanmasının yapılması 
     // ve api'den data isteğini gerçekleştiren fonksiyon
 
-    
+    stateChange();
 
     const selectedCity = select.options[select.selectedIndex].value;
+
+    const lat = coordinates[selectedCity].lat;
+
+    const lon = coordinates[selectedCity].lon;
     
-
-    const barcelonalat = barcelona.lat;
-    const barcelonalon = barcelona.lon;
-    const amsterdamlat = amsterdam.lat;
-    const amsterdamlon = amsterdam.lon;
-    const bruggelat = brugge.lat;
-    const bruggelon = brugge.lon;
-    const stockholmlat = stockholm.lat;
-    const stockholmlon = stockholm.lon;
-    const praguelat = prague.lat;
-    const praguelon = prague.lon;
-
-    const lat = (selectedCity === "barcelona") ? barcelonalat : null ||
-     (selectedCity === "amsterdam") ? amsterdamlat : null ||
-     (selectedCity === "brugge") ? bruggelat : null ||
-     (selectedCity === "stockholm") ? stockholmlat : null ||
-     (selectedCity === "prague") ? praguelat : null;
-
-    const lon = (selectedCity === "barcelona") ? barcelonalon : null ||
-    (selectedCity === "amsterdam") ? amsterdamlon : null ||
-    (selectedCity === "brugge") ? bruggelon : null ||
-    (selectedCity === "stockholm") ? stockholmlon : null ||
-    (selectedCity === "prague") ? praguelon : null;;
-
     const api = `${url}forecast?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
 
     fetch(api)
@@ -109,19 +105,28 @@ function getFollowingDays(e) {
         return data.json()
     })
     .then(followingInfo);
-
-
-    temp.setAttribute('style','font-size: 100px;');
-
 }
 
 function followingInfo (nextdays) {
-    desc.textContent = "Tomorrow " + Math.round(nextdays.list[0].main.temp) + " °C";
-    desc.setAttribute('style','font-size: 32px;');
+    
+    tomorrowtemp.textContent = "Tomorrow " + Math.round(nextdays.list[0].main.temp) + " °C";
+    
+    othertomorrowtemp.textContent = "The day after tomorrow " + Math.round(nextdays.list[1].main.temp) + " °C";
+   
+}
 
-    wind.textContent = "The day after tomorrow " + Math.round(nextdays.list[1].main.temp) + " °C";
-    wind.setAttribute('style','font-size: 28px;');
 
-    follow.textContent = "";
-    temp.textContent = "";
+function stateChange(){
+    state = "tomorrow";
+
+    if(state === "tomorrow"){
+        tomorrow.style.display = "block";
+        today.style.display = "none";
+        
+        
+    }else{
+        tomorrow.style.display = "none";
+        today.style.display = "block";
+        
+    }
 }
